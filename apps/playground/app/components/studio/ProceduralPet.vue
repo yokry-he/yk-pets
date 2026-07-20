54d393bf1ea10d912da23e07b7d25fc3bfcce7a2
Yokry He <55430512+yokry-he@users.noreply.github.com>
2026-07-21 05:00:41 +0800

    修复通用物种渲染类型

--- apps/playground/app/components/studio/ProceduralPet.vue
+++ apps/playground/app/components/studio/ProceduralPet.vue
@@ -1,11 +1,15 @@
+<!--
+  文件职责 / File responsibility
+  根据物种注册信息将通用预览分发到对应宠物渲染器。
+  Dispatches the generic preview to the appropriate pet renderer using species registration.
+-->
 <script setup lang="ts">
 import CustomizableCloudFox from './CustomizableCloudFox.vue'
 import MoonCat from './MoonCat.vue'
 import { PET_SPECIES_REGISTRY, resolveSpeciesBehavior, type MultiSpeciesAppearanceRecipe } from '~/domain/pet-species-registry'
-import type { CloudFoxStudioBehavior, CloudFoxStudioView, PetStudioAppearanceRecipe } from '~/domain/pet-studio-phase4'
+import type { CloudFoxStudioBehavior, CloudFoxStudioView } from '~/domain/pet-studio-phase4'
 const props=defineProps<{appearance:MultiSpeciesAppearanceRecipe;behavior:CloudFoxStudioBehavior;view:CloudFoxStudioView}>()
 const definition=computed(()=>PET_SPECIES_REGISTRY[props.appearance.speciesId])
 const behavior=computed(()=>resolveSpeciesBehavior(props.appearance.speciesId,props.behavior))
-const cloudAppearance=computed(()=>props.appearance as unknown as PetStudioAppearanceRecipe)
 </script>
-<template><CustomizableCloudFox v-if="appearance.speciesId==='cloud-fox'" :appearance="cloudAppearance" :behavior="behavior" :view="view"/><MoonCat v-else-if="appearance.speciesId==='moon-cat'" :appearance="appearance" :behavior="behavior" :view="view"/><TresGroup v-else><TresMesh><TresSphereGeometry :args="[.85,32,32]"/><TresMeshStandardMaterial :color="appearance.palette.coat" :emissive="appearance.palette.primaryGlow" :emissive-intensity=".35"/></TresMesh></TresGroup><slot name="species-meta" :definition="definition"/></template>
+<template><CustomizableCloudFox v-if="appearance.speciesId==='cloud-fox'" :appearance="appearance" :behavior="behavior" :view="view"/><MoonCat v-else-if="appearance.speciesId==='moon-cat'" :appearance="appearance" :behavior="behavior" :view="view"/><TresGroup v-else><TresMesh><TresSphereGeometry :args="[.85,32,32]"/><TresMeshStandardMaterial :color="appearance.palette.coat" :emissive="appearance.palette.primaryGlow" :emissive-intensity=".35"/></TresMesh></TresGroup><slot name="species-meta" :definition="definition"/></template>
